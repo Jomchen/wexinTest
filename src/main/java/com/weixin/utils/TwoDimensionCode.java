49e9f3fae62c7168d2c294b890fcfd6d526de167
zpc <937574740@qq.com>
2017-07-27 15:03:26 +0800

    增加了二维码的输出型式

--- src/main/java/com/weixin/utils/TwoDimensionCode.java
+++ src/main/java/com/weixin/utils/TwoDimensionCode.java
@@ -6,6 +6,7 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.weixin.common.MyRunTimeExcption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +18,10 @@ import java.util.Hashtable;
  * Created by zpc on 2017/7/6.
  */
 public class TwoDimensionCode {
+
+    public static Byte TARGET_TO_OUTPUTSTREAM = 0;
+
+    public static Byte TARGET_TO_PICTURE = 1;
 
     private static Logger logger = LoggerFactory.getLogger(TwoDimensionCode.class);
 
@@ -40,19 +45,26 @@ public class TwoDimensionCode {
 
 
     /**
+     *
+     * /**
      * 二维码生成嚣
      * @param content [ 二维码的内容 ]
      * @param width [ 图片宽度 ]
      * @param height [ 图片高度 ]
      * @param outputStream [ 二维码输出流 ]
+     * @param targetTo [ 二维码输出型式 ]
      */
     public static void toImage(
             String content,
             Integer width,
             Integer height,
             PicTypeEnum picTypeEnum,
-            OutputStream outputStream) {
+            OutputStream outputStream,
+            Byte targetTo) {
 
+        if (targetTo != 0 && targetTo != 1) {
+            throw new MyRunTimeExcption("请选择二维码类型");
+        }
         if (null == width || width <= 0) {
             width = TwoDimensionCode.PIC_WIDTH;
         }
@@ -70,6 +82,9 @@ public class TwoDimensionCode {
         try {
             bitMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, width, height, hints);
             MatrixToImageWriter.writeToStream(bitMatrix, picTypeEnum.getPicTypeName(), outputStream);
+            if(targetTo == TwoDimensionCode.TARGET_TO_PICTURE) {
+                outputStream.close();
+            }
         } catch (WriterException e) {
             logger.warn("【生成二维码失败。。。。。。】");
             e.printStackTrace();
